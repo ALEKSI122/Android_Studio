eb58f5b6e80214b57d310c93c9358bb0da19134b
ALEKSI122 <levadnyjaleksandr6@gmail.com>
2024-10-28 21:02:09 +0900

    111

---
+++ app/src/main/java/com/example/prkr/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.prkr
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.prkr.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btn : Button = binding.btn1
+        btn.setOnClickListener{
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("Name", binding.editText.text.toString())
+            intent.putExtra("num", 122)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/prkr/MainActivity2.kt
@@ -0,0 +1,33 @@
+package com.example.prkr
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.prkr.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val text : TextView = binding.name
+        var dann = "Получено:\n"
+        dann += intent.getStringExtra("Name") + "\n"
+        dann += intent.getIntExtra("num",0).toString()
+        text.text = dann
+    }
+}
